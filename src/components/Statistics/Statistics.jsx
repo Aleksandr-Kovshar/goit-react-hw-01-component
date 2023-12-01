0345f08dc9f9174df6821121ddf24ed49bdaf9f1
Oleksandr Kovshar <aleksandr.mpro@gmail.com>
2023-12-01 23:27:09 +0200

    task2 no props
    task2 done, but no props

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,24 @@
+// import PropTypes from "prop-types";
+import css from "./Statistics.module.css";
+
+export const Statistics = ({ stats }) => {
+  return (
+    <section className={css.statistics}>
+      <h2 className={css.title}>Upload stats</h2>
+
+      <ul className={css.statList}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            className={css.item}
+            key={id}
+            label={label}
+            percentage={percentage}
+          >
+            <span className={css.label}>{label}</span>
+            <span className={css.percentage}>{percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
