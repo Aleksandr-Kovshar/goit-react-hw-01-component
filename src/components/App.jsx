1e09c8311fd0e80e18870f36605039adc1ddbd03
Oleksandr Kovshar <aleksandr.mpro@gmail.com>
2023-12-05 22:55:54 +0200

    task 4
    add task 4 , add css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import { Profile } from "components/Profile/Profile";
 import { Statistics } from "components/Statistics/Statistics";
 import { FriendList } from "components/FriendList/FriendList";
+import { TransactionHistory } from "components/TransactionHistory/TransactionHistory";
 import user from "../user.json";
 import data from "../data.json";
 import friendsList from "../friends.json";
+import transactions from "../transactions.json";
 
 export const App = () => {
   return (
@@ -18,6 +20,7 @@ export const App = () => {
       />
       <Statistics stats={data} />
       <FriendList friends={friendsList} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
---
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -0,0 +1,26 @@
+import PropTypes from "prop-types";
+import css from "./TransactionHistory.module.css";
+
+export const TransactionHistory = ({ items }) => {
+  return (
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id} type={type} amount={amount} currency={currency}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
