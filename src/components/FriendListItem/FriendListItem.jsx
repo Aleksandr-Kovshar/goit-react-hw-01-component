df562c7de929c6ddad6644c00686c62850ccdd6a
Oleksandr Kovshar <aleksandr.mpro@gmail.com>
2023-12-14 20:35:13 +0200

    add style and isOnline

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -4,7 +4,9 @@ import css from "./FriendListItem.module.css";
 export const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
     <li className={css.item}>
-      <span className={css.status}>{`${isOnline}`}</span>
+      <span className={`${css.status} ${isOnline ? css.online : css.offline}`}>
+        {" "}
+      </span>
       <img
         className={css.avatar}
         src={avatar}
